290c3240cd97230b2d2ec1fbad4d4db110851662
Christine Joyce Belen <christinejoycebelen@gmail.com>
2025-11-27 14:41:44 +0800

    Swtiched to port 465

--- utils/email.js
+++ utils/email.js
@@ -4,12 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 
 export const sendEmail = async (to, subject, text, html = null) => {
   console.log('Attempting to send email to:', to, 'Subject:', subject);
